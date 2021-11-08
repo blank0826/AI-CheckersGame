54e08e21914b352d9deb265b2c5c7fe92be5655c
Aditya Srivastava <33955028+blank0826@users.noreply.github.com>
2021-11-08 13:28:05 +0530

    Delete CheckersGame directory

--- CheckersGame/PieceLocation.java
+++
@@ -1,9 +0,0 @@
-package CheckersGame;
-
-public class PieceLocation {
-    public Pieces pieces; //piece in checker
-    public int a; //current Location at A
-    public int b; //current Location at B
-    public boolean left; //checks if any piece in left to capture
-    public boolean right; //checks if any piece in left to capture
-}
--- CheckersGame/PieceType.java
+++
@@ -1,8 +0,0 @@
-package CheckersGame;
-
-public enum PieceType {
-   BLACK_REGULAR, //Normal Black
-   BLACK_KING,  //King Black
-   WHITE_REGULAR,  // Normal White
-   WHITE_KING   //King White
-}
